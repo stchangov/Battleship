8c9b11d2689b7bf31a0a7df31db36f587429a798
Tong02045440 <tongsean2004@gmail.com>
2025-12-01 03:15:49 -0500

    copy UI logic from placement to gameplay

--- app/src/main/java/com/mobileapp/battleship/GameplayFragment.kt
+++ app/src/main/java/com/mobileapp/battleship/GameplayFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -37,11 +38,49 @@ class GameplayFragment : Fragment() {
         _binding = FragmentGameplayBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        //val gameBoard = binding.gridGameBoard
-        //setupBoard(gameBoard)
+        val gameBoard = binding.gridGameplay
+        setupBoard(gameBoard)
 
 
 
         return view
+    }
+
+    private fun setupBoard(gameBoard: GridLayout) {
+        val size = 10
+        // initialize the tiles
+        tileButtons = Array(10) { arrayOfNulls<ImageView>(10) }
+
+        for (row in 0 until size) {
+            for (col in 0 until size) {
+                // Create the tile and apply its initial appearance and behavior
+                val tile = ImageView(requireContext()).apply {
+                    setImageResource(R.drawable.circle)
+                    scaleType = ImageView.ScaleType.CENTER_INSIDE
+
+                    val tileSize = (37.3 * resources.displayMetrics.density).toInt()
+                    val margin = (2 * resources.displayMetrics.density).toInt()
+
+                    layoutParams = GridLayout.LayoutParams().apply {
+                        width = tileSize
+                        height = tileSize
+                        rowSpec = GridLayout.spec(row)
+                        columnSpec = GridLayout.spec(col)
+                        setMargins(margin, margin, margin, margin)
+                    }
+
+                    // Attach listener
+                    setOnClickListener {
+                        //onTileClicked(row, col)
+                    }
+                }
+
+                // Store this tile
+                tileButtons[row][col] = tile
+
+                // Add the tile to the GridLayout so it becomes visible
+                gameBoard.addView(tile)
+            }
+        }
     }
 }
