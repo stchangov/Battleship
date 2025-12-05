f397072faaf61a005d64d7dd35c8306a5320adf2
Sean_Tong <tongsean2004@gmail.com>
2025-12-05 06:16:40 -0500

    add temp fix for destroying ViewModel

--- app/src/main/java/com/mobileapp/battleship/GameOverFragment.kt
+++ app/src/main/java/com/mobileapp/battleship/GameOverFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.mobileapp.battleship.databinding.FragmentGameOverBinding
 
@@ -24,6 +25,16 @@ class GameOverFragment : Fragment() {
     ): View {
         _binding = FragmentGameOverBinding.inflate(inflater,container,false)
 
+        binding.backMenuButton.setOnClickListener {
+            /*TODO
+            *  Currently the game ViewModel does not get destroyed,
+            *  so as a temp solution this will clear all ViewModels.
+            *  Need to find a way to only clear the game ViewModel.
+            * */
+            activity?.viewModelStore?.clear()
+            findNavController().navigate(R.id.action_gameOverFragment_to_mainMenuFragment)
+        }
+
         val res = resources
 
         binding.playerWinTextBox.text = getString(R.string.who_won_text, if (args.winner == Player.PLAYER1) 1 else 2)
@@ -31,6 +42,8 @@ class GameOverFragment : Fragment() {
         binding.player1MissBox.text = res.getQuantityString(R.plurals.misses_p1_text, args.missMadeByP1, args.missMadeByP1)
         binding.player2HitsBox.text = res.getQuantityString(R.plurals.hits_p2_text, args.hitsMadeByP2, args.hitsMadeByP2)
         binding.player2MissBox.text = res.getQuantityString(R.plurals.misses_p2_text, args.missMadeByP2, args.missMadeByP2)
+
+
 
         return binding.root
     }
