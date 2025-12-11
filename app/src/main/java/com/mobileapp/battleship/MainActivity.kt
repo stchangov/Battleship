4dc475d91e0d43bf0eba02bdd563fe23806f7da8
Tong02045440 <tongsean2004@gmail.com>
2025-12-11 18:13:49 -0500

    add honor statement and header

--- app/src/main/java/com/mobileapp/battleship/MainActivity.kt
+++ app/src/main/java/com/mobileapp/battleship/MainActivity.kt
@@ -1,3 +1,45 @@
+/***********************************************
+Authors: Luca Azevedo, Sean Tong, Stephan Tchangov
+Date: 12/11/2025
+Purpose: Our project is to build a turn-based, pass-and-play Battleship app
+that allows two players to set up boards; exchange turns and complete a full
+game using a single device. The mission is to create a simple, accessible, and
+engaging mobile experience that brings the classic strategy game to Android without
+requiring accounts or online connectivity.
+What Learned:
+    Android Fragment Architecture
+    Navigation Graph & ViewBinding
+    Team Git Workflow
+    UI/UX Design Consistency
+    Android Animations
+    Data & File Saving
+Sources of Help:
+    Blackboard Class Learning Materials
+    https://kotlinlang.org/docs/home.html
+    https://developer.android.com/reference
+Time Spent (Hours): 62 hours
+ ***********************************************/
+
+/*
+Mobile App Development I -- COMP.4630 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order
+in the classroom, providing an enriching learning experience for students,
+and training as a practicing computing professional upon graduation. This
+practice is manifested in the University's Academic Integrity policy.
+Students are expected to strictly avoid academic dishonesty and adhere to the
+Academic Integrity policy as outlined in the course catalog. Violations will
+be dealt with as outlined therein. All programming assignments in this class
+are to be done by the student alone unless otherwise specified. No outside
+help is permitted except the instructor and approved tutors.
+I
+certify that the work submitted with this assignment is mine and was
+generated in a manner consistent with this document, the course academic
+policy on the course website on Blackboard, and the UMass Lowell academic
+code.
+Date: 12/11/2025
+Names: Luca Azevedo, Sean Tong, Stephan Tchangov
+*/
+
 package com.mobileapp.battleship
 
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/mobileapp/battleship/StatsViewModel.kt
+++ app/src/main/java/com/mobileapp/battleship/StatsViewModel.kt
@@ -7,6 +7,7 @@ import java.io.File
 class StatsViewModel : ViewModel() {
     private val fileName = "stats.txt"
 
+    // get the contents from the file as a string, other wise if it doesnt exist, return empty string
     fun readFromFile(context: Context): String {
         val file = File(context.filesDir, fileName)
         if(!file.exists()) {
